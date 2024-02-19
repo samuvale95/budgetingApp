e4e57aef33004925dcbc2987d63905d2719d7b81
Sule <samuelevalente@gmail.com>
2024-02-19 10:17:07 +0100

    add translate service

--- budgeting-app/src/app/app.component.ts
+++ budgeting-app/src/app/app.component.ts
@@ -1,13 +1,41 @@
-import { Component } from '@angular/core';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { Component, importProvidersFrom } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
+export function HttpLoaderFactory(httpClient: HttpClient){
+  return new TranslateHttpLoader(httpClient);
+}
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TranslateModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'budgeting-app';
+
+  constructor (public translate: TranslateService) {
+    translate.addLangs(['it']);
+    translate.setDefaultLang('it');
+    translate.use('it');
+  }
 }
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideHttpClient(),
+    importProvidersFrom(
+      TranslateModule.forRoot({
+          loader:{
+            provide: TranslateLoader,
+            useFactory: HttpLoaderFactory,
+            deps: [HttpClient]
+          }
+        })
+    )
+  ]
+})
