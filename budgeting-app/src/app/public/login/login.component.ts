f5d0185ee8cfbf633aef2f23a844e5fe662377f8
Sule <samuelevalente@gmail.com>
2024-02-26 21:29:30 +0100

    add login template

--- budgeting-app/src/app/public/login/login.component.ts
+++ budgeting-app/src/app/public/login/login.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { MatDividerModule } from '@angular/material/divider';
+import { SvgComponent } from "../../shared/svg/svg.component";
 
 @Component({
-  selector: 'app-login',
-  standalone: true,
-  imports: [],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+    selector: 'app-login',
+    standalone: true,
+    templateUrl: './login.component.html',
+    styleUrl: './login.component.scss',
+    imports: [MatDividerModule, SvgComponent]
 })
 export class LoginComponent {
 
