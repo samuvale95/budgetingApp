060e4773e6b8a6ec473a7c0c146b687a0b90394c
Sule <samuelevalente@gmail.com>
2024-03-05 01:01:42 +0100

    fix form group

--- budgeting-app/src/app/public/login/login.component.ts
+++ budgeting-app/src/app/public/login/login.component.ts
@@ -2,12 +2,36 @@ import { Component } from '@angular/core';
 import { MatDividerModule } from '@angular/material/divider';
 import { SvgComponent } from "../../shared/svg/svg.component";
 import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { PublicActions } from '../store/public.actions';
+import { Store } from '@ngrx/store';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-login',
     standalone: true,
     templateUrl: './login.component.html',
     styleUrl: './login.component.scss',
-    imports: [MatDividerModule, SvgComponent, RouterOutlet, RouterLink]
+    imports: [MatDividerModule, SvgComponent, RouterOutlet, RouterLink, ReactiveFormsModule]
 })
-export class LoginComponent {}
+export class LoginComponent {
+
+    loginForm: FormGroup = this.fb.group({
+        email: ['', Validators.required],
+        password: ['', Validators.required]
+    });
+
+    constructor(
+        private store: Store,
+        private fb: FormBuilder
+    )
+    {}
+
+    login() {
+        if(this.loginForm.valid){
+            this.store.dispatch(PublicActions.login({
+                email: this.loginForm.get('email')?.value, 
+                password:this.loginForm.get('password')?.value
+            }));
+        }
+    }
+}
