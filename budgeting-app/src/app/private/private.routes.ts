d883e14b6cf17ca2ad9ea72a824e9cc56db81491
Sule <samuelevalente@gmail.com>
2024-03-13 15:58:08 +0100

    move component into private folder

--- budgeting-app/src/app/private/private.routes.ts
+++ budgeting-app/src/app/private/private.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
-import { PrivateComponent } from './private.component';
-import { HorizontalBarsComponent } from '../shared/components/horizontal-bars/horizontal-bars.component';
+import { HorizontalBarsComponent } from './components/horizontal-bars/horizontal-bars.component';
 import { provideState } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 import { privateFeature } from './store/private.reducer';
