3b55d0f22ef36b69b47e32665d4b50110d9c1e84
Sule <samuelevalente@gmail.com>
2024-04-06 00:54:23 +0200

    add custom button

---
+++ budgeting-app/src/app/shared/components/custom-button/custom-button.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './custom-button.component.html',
+  styleUrl: './custom-button.component.scss'
+})
+export class CustomButtonComponent {
+  @Input() public title: string = '';
+  @Input() public onClick: ()=>void = ()=>{};
+}
