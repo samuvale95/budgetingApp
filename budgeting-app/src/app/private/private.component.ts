40eb10db025a4218e1ddeb72742215a72a42d9ba
Sule <samuelevalente@gmail.com>
2024-04-06 00:55:04 +0200

    refactor private route and add navbar

--- budgeting-app/src/app/private/private.component.ts
+++ budgeting-app/src/app/private/private.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../shared/header/header.component";
 import { NavMenuComponent } from "../shared/nav-menu/nav-menu.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
     selector: 'app-private',
     standalone: true,
     templateUrl: './private.component.html',
     styleUrl: './private.component.scss',
-    imports: [HeaderComponent, NavMenuComponent]
+    imports: [HeaderComponent, NavMenuComponent, RouterOutlet]
 })
 export class PrivateComponent {
 
--- budgeting-app/src/app/private/private.routes.ts
+++ budgeting-app/src/app/private/private.routes.ts
@@ -3,15 +3,58 @@ import { provideState } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 import { privateFeature } from './store/private.reducer';
 import { PrivateEffects } from './store/private.effects';
-import { CustomSelectButtonComponent } from './components/custom-select/custom-select.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TransactionsComponent } from './transactions/transactions.component';
+import { PlanningComponent } from './planning/planning.component';
+import { GraphsComponent } from './graphs/graphs.component';
+import { SettingsComponent } from './settings/settings.component';
+import { PrivateComponent } from './private.component';
 
 export const privateRoutes: Routes = [
     {
-        path:'component',
-        component: CustomSelectButtonComponent,
-        providers:[
-            provideState(privateFeature),
-            provideEffects(PrivateEffects)
+        path: '',
+        component: PrivateComponent,
+        children: [
+            {
+                path:'dashboard',
+                component: DashboardComponent,
+                providers:[
+                    provideState(privateFeature),
+                    provideEffects(PrivateEffects)
+                ]
+            },
+            {
+                path:'transactions',
+                component: TransactionsComponent,
+                providers:[
+                    provideState(privateFeature),
+                    provideEffects(PrivateEffects)
+                ]
+            },
+            {
+                path:'planning',
+                component: PlanningComponent,
+                providers:[
+                    provideState(privateFeature),
+                    provideEffects(PrivateEffects)
+                ]
+            },
+            {
+                path:'graphs',
+                component: GraphsComponent,
+                providers:[
+                    provideState(privateFeature),
+                    provideEffects(PrivateEffects)
+                ]
+            },
+            {
+                path:'setting',
+                component: SettingsComponent,
+                providers:[
+                    provideState(privateFeature),
+                    provideEffects(PrivateEffects)
+                ]
+            }
         ]
     }
 ];
